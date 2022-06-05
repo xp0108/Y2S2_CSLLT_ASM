632da9e1412fd1afe71f72f551d4e14f28516de4
xp <74288201+xp0108@users.noreply.github.com>
2022-06-05 14:06:28 +0800

    modi nl

--- Assignment/CSLLTModi.asm
+++ Assignment/CSLLTModi.asm
@@ -69,6 +69,14 @@ printDesAndExit Macro des
     int 21h
 EndM
 
+PrintColorChar Macro
+mov bh, 0   ;Display page
+    mov ah, 02h ;SetCursorPosition
+    int 10h
+    mov ah, 09h
+    int 10h
+EndM
+
 Main proc
     
     ;fetch data
@@ -535,148 +543,196 @@ BoxInnerRight PROC
 BoxInnerRight endp
 
 NLoop PROC
+    mov ah,2
+    call TopLeftAstTriangle
+    mov ah,2
+    call TopRightAstTriangle
+    mov ah,2
+    call BottomLeftNumTriangle
+    mov ah,2
+    call BottomRightNumTriangle
 
-    ;if invalid input 
-    jmp LoopStart ;skip invalid input first
-    LoopInvalid:
-    
-    ;print new line
-    newLine
-    
-    ;print invalid
-    mov ah, 09h
-    mov dx, offset pInvalid
-    int 21h
-    jmp LoopStart
-
-    printDesAndExit pLoop
-    LoopStart:
-    printDesAndExit loopInput
-
-    ;accept input
-    mov ah, 1 
-    int 21h 
-    ;convert input into ASCII
-    cbw
-    mov  di, ax
-    cmp di, '3'
-    jl LoopInvalid
-
-    cmp di, '9'
-    jg LoopInvalid
-    sub di, 48      ;move ASCII
-
-    clearScreenDefineColor 74h
-
-;TOP LEFT------------------------------------------------------------
-    mov dh,1 ;row
-    mov dl,0 ;col
-    mov cx,di
-    StartTopLeft:
-        push cx
-        StartTopLeftPrint:
-            mov tempColor, 4fh ;define color
-            mov bl,tempColor
-            mov al, '*'
-            mov ah, 2     ;cursor start at 0
-            int 10h
-            mov ah, 9     ; print horizontal
-            int 10h
-        loop StartTopLeftPrint
-
-        inc dh
-        pop cx
-    loop StartTopLeft
-
-;TOP RIGHT------------------------------------------------------------
-
-    mov dh,1 ;row
-    mov dl,18 ;col
-    mov cx,di ;row count
-    mov si,1
-    StartTopRight:
-        push cx 
-            mov cx, si ;cx=1
-            StartTopRightPrint:
-                mov tempColor, 4ch ;define color
-                mov bl,tempColor
-                mov al,42
-                mov ah, 2     ;cursor start at 0
-                int 10h
-                mov ah, 9     ; print horizontal
-                int 10h
-            loop StartTopRightPrint
-        inc si ;bx+1=2
-        
-        inc dh  ;nextline
-        pop cx  ;4
-    loop StartTopRight
-
-;BOTTOM LEFT-------------------------------------------------------
-
-    mov dh,14    ;row
-    mov dl,0    ;column
-    mov cx,di ;row ,i=5
-    mov si, 1 ;j = column
-    ;num = tempVar
-    NumberBottomLeft:
-
-        push cx
-        mov cx, si ;cx=1
-        mov tempColor, 4ch ;define color
-        mov bl,tempColor
-        mov al, "1"
-    
-        mov tempVar,al
-
-        NumberBottomLeftPrint:   
-            mov al, tempVar                 
-            mov bh, 0
-            mov ah, 2 ;set cursor position
-            int 10h
-            mov ah, 09h ;display chara
-            int 10h     ;call BIOS
-            inc al
-            mov tempVar,al
-        loop NumberBottomLeftPrint
-        
-        inc si ;bx+1=2
-        
-        pop cx  ;4
-
-        inc dh ;inc row
-    loop NumberBottomLeft
-
-;BOTTOM RIGHT-----------------------------------------
-    mov dh,14    ;row
-    mov dl,18    ;column
-    mov cx,di ;row count ,i=5
-    NumberBottomRight:
-
-        push cx
-        mov tempColor, 4fh ;define color
-        mov bl,tempColor
-        mov al,"1"
-        mov tempVar,al
-        NumberBottomRightPrint:   
-            mov al, tempVar                 
-            mov bh, 0
-            mov ah, 2 ;set cursor position
-            int 10h
-            mov ah, 09h ;display chara
-            int 10h     ;call BIOS
-            inc al
-            mov tempVar,al
-        loop NumberBottomRightPrint
-        pop cx  ;4
-
-        inc dh  ;new row/newline
-    loop NumberBottomRight
-    
     jmp exit
     ret
 NLoop endp
 
+TopLeftAstTriangle proc  
+
+    clearScreenDefineColor 0fh
+    
+    mov tempColor, 4fh ;define color
+    mov dh,1 ;row
+    mov dl,0 ;col
+    mov cx,1 ;j=1
+    mov bx,5 ;i=5
+    jmp TopLeftAstTriangleStart
+    
+    TopLeftAstTriangleElse:                    
+        dec bx 
+        CMP bx,4
+        je TopLeftAstTriangleIf
+        inc dh
+        mov dl,0
+        
+    TopLeftAstTriangleIf:
+        push dx
+        push cx
+        push bx
+        
+        mov bl,tempColor
+        mov al,"*"
+        mov cx,1
+        PrintColorChar
+        
+        pop bx
+        pop cx
+        pop dx
+        inc dl
+    
+    TopLeftAstTriangleStart:
+        loop TopLeftAstTriangleIf
+        
+        mov cx,7
+        sub cx,bx
+        CMP bx,0
+        je TopLeftAstTriangleEnd ;jmp to end
+        
+        loop TopLeftAstTriangleElse 
+        
+    TopLeftAstTriangleEnd:
+    ret
+    
+TopLeftAstTriangle endp
+    
+TopRightAstTriangle proc  
+    mov tempColor, 4ch ;define color
+    mov cx,6
+    mov bx,5
+    mov dh,1
+    mov dl,9
+    jmp TopRightAstTriangleStart
+    
+    TopRightAstTriangleElse:
+        dec bx
+        mov dl,9
+        inc dh
+    
+    
+    TopRightAstTriangleIf:
+        push dx
+        push cx
+        push bx
+        
+        mov bl,tempColor
+        mov al,"*"
+        mov cx,1
+        PrintColorChar
+        
+        pop bx
+        pop cx
+        pop dx
+        inc dl
+    
+    
+    TopRightAstTriangleStart:
+        loop TopRightAstTriangleIf
+        mov cx,bx
+        loop TopRightAstTriangleElse
+    
+    ret
+TopRightAstTriangle endp   
+
+BottomLeftNumTriangle proc  
+    mov tempColor, 4ch ;define color
+    mov dh,7
+    mov dl,0
+    mov cx,1
+    mov bx,5  
+    mov al,49
+    mov tempVar,al  ;Number
+    jmp BottomLeftNumTriangleStart
+    
+    BottomLeftNumTriangleElse: 
+        dec bx
+        CMP bx,4
+        je BottomLeftNumTriangleIf
+        inc dh
+        mov dl,0
+    
+    BottomLeftNumTriangleIf:
+        push dx
+        push cx
+        push bx
+        
+        mov bl,tempColor
+        mov al,tempVar  ;Number
+        mov cx,1
+        PrintColorChar
+        
+        pop bx
+        pop cx
+        pop dx
+        inc dl
+        inc tempVar ;Number
+    
+    BottomLeftNumTriangleStart:
+        loop BottomLeftNumTriangleIf
+        
+        mov cx,7
+        sub cx,bx
+        CMP bx,0
+        je BottomLeftNumTriangleEnd ;jmp to end
+        
+        mov tempVar,49  ;Number
+        loop BottomLeftNumTriangleElse
+        BottomLeftNumTriangleEnd:
+    
+    ret
+    
+BottomLeftNumTriangle endp
+
+BottomRightNumTriangle proc  
+    mov tempColor, 4fh ;define color
+    mov cx,6
+    mov bx,5
+    mov dh,7
+    mov dl,9
+    mov al,49
+    mov tempVar,al  ;Number
+    jmp BottomRightNumTriangleStart
+    
+    BottomRightNumTriangleElse:
+        dec bx
+        ; CALL Newline
+        mov dl,9
+        inc dh
+    
+    
+    BottomRightNumTriangleIf:
+        push dx
+        push cx
+        push bx
+        
+        mov bl,tempColor
+        mov al,tempVar  ;Number
+        mov cx,1
+        PrintColorChar
+        
+        pop bx
+        pop cx
+        pop dx
+        inc dl
+        inc tempVar ;Number
+    
+    
+    BottomRightNumTriangleStart:
+        loop BottomRightNumTriangleIf
+        mov cx,bx  
+        mov tempVar,49 ;Number
+        loop BottomRightNumTriangleElse
+    
+    ret
+BottomRightNumTriangle endp
 
 end main
 
--- Assignment/test3.asm
+++ Assignment/test3.asm
@@ -1,88 +1,13 @@
-
-
-; .model small
-; .stack 100h
-; .data
-; .code
-; main proc
-    
-;     MOV CX,5
-;     MOV BX,5
-    
-    
-;     L1:
-;     PUSH CX
-;     MOV CX,5
-       
-;     L2:
-;      MOV AH,2
-;      MOV DL,'*'
-;      INT 21H
-;      MOV DL,' '
-;      INT 21H
-       
-;      LOOP L2
-     
-;      MOV AH,2
-;      MOV DL,10
-;      INT 21H
-;      MOV DL,13
-;      INT 21H
-     
-     
-;     POP CX     
-       
-       
-;     LOOP L1
-       
-       
-    
-;     EXIT:
-;     MOV AH,4CH
-;     INT 21H
-
-; main endp
-; end main
-
-
 .model small
 .stack 100h
 .data 
-    menu    db 10,10,13,"!!!!! HAPPY INDEPENDENCE DAY, MALAYSIA !!!!!"
-        db 10,10,13,6,0,6,0,6,0,6,0,"MAIN MENU",0,6,0,6,0,6,0,6,0,13,10,10
-        db "1. Number Patterns",13,10
-        db "2. Design Patterns",13,10
-        db "3. Box Type Patterns",13,10
-        db "4. Nested Loop Patterns",13,10
-        db "5. Exit Program",13,10,10
-        db "Select your choice: $"  
-        
-    invalidInputMsg db 10,10,"########## INVALID INPUT! ENTER AGAIN ##########",13,10,'$'
-
-    msg1 db "here is number patterns",10,10,'$' 
-    msg2 db "here is Design patterns",10,10,'$' 
-    msg3 db "here is Box type patterns",10,10,'$'
-    msg4 db "here is Nested loop patterns",10,10,'$'
-    
-    wannaContinue db 10,10,13, "Do you wanted to continue the program [Y/N] ? " ,0, '$'
-
-    displayNoPttrn db "How many row you wanted to display [4~9]? $"
-    colour db ?
     tempVar db ?
-    tempVar2 db ?
-.code 
-MacroDisMsg Macro msg
-    lea dx, msg  
-    mov ah, 09h 
-    int 21h  
-EndM
+    tempColor db 9
+    char db ?
+    count db ?
+.code   
 
-MacroAcceptChar Macro
-    mov ah, 1           ;read a letter
-    int 21h 
-    ; mov bl,al           ;move al to bl  
-EndM
-MacroClearScreen Macro colorVar
+clearScreenDefineColor Macro colorVar
     mov ah,6        ;clear screen
     mov al,0        ;Full Screen
     mov bh,colorVar ;4fh color
@@ -98,127 +23,207 @@ MacroClearScreen Macro colorVar
     int 10h
 EndM
 
+PrintColorChar Macro
+mov bh, 0   ;Display page
+    mov ah, 02h ;SetCursorPosition
+    int 10h
+    mov ah, 09h
+    int 10h
+EndM
 main proc  
-;-------------------------
-;UPPER SQUARE
-    mov bl, 1 ;i=1, int i = 1
-    UpperSquareOutter:
-    ;UPPER SQUARE _ INNER LEFT
-    USIL: call SquareInnerLeft
 
-    ;UPPER SQUARE _ INNER RIGHT
-    USIR: call SquareInnerRight
-
-;-----------INNER LOOP END--------------
-    ExitUpperSquareOutter:
-    
+   
     mov ah,2
-    mov dl,10
-    int 21h
-
-    inc bl
-    cmp bl, 5
-    jne UpperSquareOutter
-
-;---------UPPER SQUARE END----------------
-
-;-------------------------
-;LOWER SQUARE
-    mov bl, 3 ;i=1, int i = 1
-    LowerSquareOutter:
-    ;LOWER SQUARE _ INNER LEFT
-    LSIL: call SquareInnerLeft
-
-    ;LOWER SQUARE _ INNER RIGHT
-    LSIR: call SquareInnerRight
-    
-    ExitLowerSquareOutter:
+    call TopLeftAstTriangle
     mov ah,2
-    mov dl,10
-    int 21h
+    call TopRightAstTriangle
+    mov ah,2
+    call BottomLeftNumTriangle
+    mov ah,2
+    call BottomRightNumTriangle
 
-    dec bl
-    cmp bl, 0
-    jne LowerSquareOutter
-;-------------------------
 
-SquaEnd:
+NLoopEnd:
     mov ah,4ch 
-    int 21h
+    int 21h 
+
 main endp
 
-SquareInnerLeft PROC
-    mov cl, 1 ;int j = 1
-    InnerLoopLeft:  
-    mov tempVar, 52 ;tempVar = n = 4
+TopLeftAstTriangle proc  
 
-    cmp bl, cl
-    jl FirstIFFF
-    ;ELSE
-    SecondIFF:
-    sub tempVar, cl ;n - j
-    inc tempVar ;+1
-    mov ah, 2
-    mov dl, tempVar
-    int 21h
-    mov dl, 0
-    int 21h
+    clearScreenDefineColor 0fh
     
-    jmp InnerLoopLeftExit
-    ;IF
-    FirstIFFF:
-    sub tempVar, bl ;n - i
-    inc tempVar ;+1
-    mov ah, 2
-    mov dl, tempVar
-    int 21h
-    mov dl, 0
-    int 21h
-
-    InnerLoopLeftExit:
-    inc cl
-    cmp cl, 5
-    jne InnerLoopLeft
-
+    mov tempColor, 4fh ;define color
+    mov dh,1 ;row
+    mov dl,0 ;col
+    mov cx,1 ;j=1
+    mov bx,5 ;i=5
+    jmp TopLeftAstTriangleStart
+    
+    TopLeftAstTriangleElse:                    
+        dec bx 
+        CMP bx,4
+        je TopLeftAstTriangleIf
+        inc dh
+        mov dl,0
+        
+    TopLeftAstTriangleIf:
+        push dx
+        push cx
+        push bx
+        
+        mov bl,tempColor
+        mov al,"*"
+        mov cx,1
+        PrintColorChar
+        
+        pop bx
+        pop cx
+        pop dx
+        inc dl
+    
+    TopLeftAstTriangleStart:
+        loop TopLeftAstTriangleIf
+        
+        mov cx,7
+        sub cx,bx
+        CMP bx,0
+        je TopLeftAstTriangleEnd ;jmp to end
+        
+        loop TopLeftAstTriangleElse 
+        
+    TopLeftAstTriangleEnd:
     ret
-SquareInnerLeft endp
-
-
-SquareInnerRight PROC
-    mov cl, 3 ;int j = 3 (n-1)
-    InnerLoopRight:
-    mov tempVar, 52 ;tempVar = n = 4
-
-    ;IF ELSE START
-    cmp bl, cl
-    jl FirstInnerUpLeftIFFF
-    ;ELSE
-    SecondInnerUpLeftIFF:
-    sub tempVar, cl ;n - j
-    inc tempVar ;+1
-    mov ah, 2
-    mov dl, tempVar
-    int 21h
-    mov dl, 0
-    int 21h
     
-    jmp InnerLoopRightExit
-    ;IF
-    FirstInnerUpLeftIFFF:
-    sub tempVar, bl ;n - i
-    inc tempVar ;+1
-    mov ah, 2
-    mov dl, tempVar
-    int 21h
-    mov dl, 0
-    int 21h
-
-    InnerLoopRightExit:
-    dec cl
-    cmp cl, 0
+TopLeftAstTriangle endp
     
-    jne InnerLoopRight
+TopRightAstTriangle proc  
+    mov tempColor, 4ch ;define color
+    mov cx,6
+    mov bx,5
+    mov dh,1
+    mov dl,9
+    jmp TopRightAstTriangleStart
+    
+    TopRightAstTriangleElse:
+        dec bx
+        mov dl,9
+        inc dh
+    
+    
+    TopRightAstTriangleIf:
+        push dx
+        push cx
+        push bx
+        
+        mov bl,tempColor
+        mov al,"*"
+        mov cx,1
+        PrintColorChar
+        
+        pop bx
+        pop cx
+        pop dx
+        inc dl
+    
+    
+    TopRightAstTriangleStart:
+        loop TopRightAstTriangleIf
+        mov cx,bx
+        loop TopRightAstTriangleElse
+    
     ret
-SquareInnerRight endp
+TopRightAstTriangle endp   
 
+BottomLeftNumTriangle proc  
+    mov tempColor, 4ch ;define color
+    mov dh,7
+    mov dl,0
+    mov cx,1
+    mov bx,5  
+    mov al,49
+    mov tempVar,al  ;Number
+    jmp BottomLeftNumTriangleStart
+    
+    BottomLeftNumTriangleElse: 
+        dec bx
+        CMP bx,4
+        je BottomLeftNumTriangleIf
+        inc dh
+        mov dl,0
+    
+    BottomLeftNumTriangleIf:
+        push dx
+        push cx
+        push bx
+        
+        mov bl,tempColor
+        mov al,tempVar  ;Number
+        mov cx,1
+        PrintColorChar
+        
+        pop bx
+        pop cx
+        pop dx
+        inc dl
+        inc tempVar ;Number
+    
+    BottomLeftNumTriangleStart:
+        loop BottomLeftNumTriangleIf
+        
+        mov cx,7
+        sub cx,bx
+        CMP bx,0
+        je BottomLeftNumTriangleEnd ;jmp to end
+        
+        mov tempVar,49  ;Number
+        loop BottomLeftNumTriangleElse
+        BottomLeftNumTriangleEnd:
+    
+    ret
+    
+BottomLeftNumTriangle endp
+
+BottomRightNumTriangle proc  
+    mov tempColor, 4fh ;define color
+    mov cx,6
+    mov bx,5
+    mov dh,7
+    mov dl,9
+    mov al,49
+    mov tempVar,al  ;Number
+    jmp BottomRightNumTriangleStart
+    
+    BottomRightNumTriangleElse:
+        dec bx
+        ; CALL Newline
+        mov dl,9
+        inc dh
+    
+    
+    BottomRightNumTriangleIf:
+        push dx
+        push cx
+        push bx
+        
+        mov bl,tempColor
+        mov al,tempVar  ;Number
+        mov cx,1
+        PrintColorChar
+        
+        pop bx
+        pop cx
+        pop dx
+        inc dl
+        inc tempVar ;Number
+    
+    
+    BottomRightNumTriangleStart:
+        loop BottomRightNumTriangleIf
+        mov cx,bx  
+        mov tempVar,49 ;Number
+        loop BottomRightNumTriangleElse
+    
+    ret
+BottomRightNumTriangle endp
 end main
